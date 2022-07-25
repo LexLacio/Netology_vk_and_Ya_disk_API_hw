18a994d7b16bdab37370ddf5abb4cbd77d250626
Александр Лацинов <a8880@mail.ru>
2022-07-25 22:42:04 +0300

    Pickups were fixed. Added req.txt and settings.ini files

--- vk_API.py
+++ vk_API.py
@@ -4,7 +4,6 @@ import time
 
 import json
 
-
 from pprint import pprint
 
 
@@ -19,19 +18,33 @@ class YandexDisk:
             'Authorization': 'OAuth {}'.format(self.token)
         }
 
+    def folder_creation(self, folder_name):
+
+        """Функция создаёт папки на ya.disk"""
+
+        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/"
+        headers = self.get_headers()
+        params = {"path": folder_name}
+        response = requests.put(upload_url, headers=headers, params=params)
+        if response.status_code == 201:
+            print(f'Папка {folder_name} cоздана на Яндекс диске')
+        else:
+            print(f'Папка {folder_name} уже есть на Яндекс диске')
+            # print(response.status_code)
+
     def ava_uploader(self, ava_url, ava_name, ya_folder):
 
         """Функция загружает аватарки по ссылке на ya.disk"""
 
         link = ava_url
         upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
-        file_path = ya_folder + str(ava_name)
+        file_path = 'disk:/' + ya_folder + '/' + str(ava_name)
         headers = self.get_headers()
         params = {"url": link, "path": file_path}
         response = requests.post(upload_url, headers=headers, params=params)
         response.raise_for_status()
         if response.status_code == 202:
-            print("Success")
+            print(f'Аватарка {ava_name} загружена на Яндекс диск')
         else:
             print(response.status_code)
 
@@ -40,15 +53,17 @@ class YandexDisk:
         headers = self.get_headers()
         params = {"path": disk_file_path, "overwrite": "true"}
         response = requests.get(upload_url, headers=headers, params=params)
-        pprint(response.json())
+        # pprint(response.json())
         return response.json()
 
-    def upload_file_to_disk(self, disk_file_path):
-        href = self._get_upload_link(disk_file_path=disk_file_path + 'ava_list.json').get("href", "")
+    def upload_file_to_disk(self, list_name, disk_file_path):
+        with open('ava_list.json', "w") as file:
+            json.dump(list_name, file, ensure_ascii=False, indent=2)
+        href = self._get_upload_link(disk_file_path='disk:/' + disk_file_path + '/ava_list.json').get("href", "")
         response = requests.put(href, data=open('ava_list.json', 'rb'))
         response.raise_for_status()
         if response.status_code == 201:
-            print("Success")
+            print('ava_list.json файл-легенда аватарок загружен на Яндекс диск')
 
 
 class VkUser:
@@ -60,7 +75,18 @@ class VkUser:
             'v': '5.131'
         }
 
-    def ava_download(self, User_id):
+    def get_user_id(self, screen_name):
+
+        """Функция получает user_id по screen_name"""
+
+        URL = 'https://api.vk.com/method/users.get'
+        user_screen_name = {'user_ids': screen_name}
+        res = requests.get(URL, params={**self.params, **user_screen_name})
+        vk_user_id = res.json()['response'][0]['id']
+        # print(f'screen_name {screen_name} соответствует user_id {vk_user_id }')
+        return vk_user_id
+
+    def ava_download(self, User_id, avas_count):
 
         """Функция скачивает аватарки по UserId и выводит результат в json"""
 
@@ -70,12 +96,13 @@ class VkUser:
             'album_id': 'profile',
             'rev': 0,
             'extended': 1,
-            'photo_sizes': 1
+            'photo_sizes': 1,
+            'count': avas_count
         }
         ava_download_result = requests.get(ava_download_url, params={**self.params, **ava_download_params}).json()
         return ava_download_result
 
-    def ava_data_editor(self, User_id, ya_token, ya_folder):
+    def ava_data_editor(self, User_id, avas_count, ya_token, ya_folder):
 
         """Функция обрабатывает json аватарок и загружает поштучно на yandex диск.
         Выводит пронумерованный словарь с значениями:
@@ -83,9 +110,10 @@ class VkUser:
         размер файла: формат файла"""
 
         ya = YandexDisk(token=ya_token)
-        ava_download_res = self.ava_download(User_id)
+        ava_download_res = self.ava_download(User_id, avas_count)
         ava_data = ava_download_res['response']['items']
-        ava_list = {}
+        ava_list = {}  # Создание словаря для json файла с легендой
+        ava_list_uniq = []  # Список для проверки уникальности имён аватарок
         counter = 0
         for ava in ava_data:
             counter += 1
@@ -94,16 +122,20 @@ class VkUser:
             for url in ava['sizes']:  # Цикл перебирает ссылки и типы и выводит самые последние ссылки с типами
                 url_list.append(url['url'])
                 url_type_list.append(url['type'])
-            ava_link = url_list[-1]   # Получаем последнюю из списка ссылку на аву
-            ava_size = url_type_list[-1]   # Получаем тип/размер авы для последней из списка
+            ava_link = url_list[-1]  # Получаем последнюю из списка ссылку на аву
+            ava_size = url_type_list[-1]  # Получаем тип/размер авы для последней из списка
             ava_date = str(time.strftime('%Y-%m-%d', time.localtime(int(ava["date"]))))
-            ava_name = str(ava["likes"]['count']) + '_' + ava_date + '.jpeg'
+            ava_name = str(ava["likes"]['count']) + '.jpeg'
+            if ava_name in ava_list_uniq:  # Проверка уникальности имён аватарок
+                ava_name = str(ava["likes"]['count']) + '_' + ava_date + '.jpeg'
+            else:
+                pass
             ya.ava_uploader(ava_link, ava_name, ya_folder)
-            ava_list[counter] = [{"file_name": ava_name, "size": ava_size}] # Создание json файла с легендой
-        with open('ava_list.json', "w") as file:
-            json.dump(ava_list, file, ensure_ascii=False, indent=2)
-        ya.upload_file_to_disk(ya_folder)  # Загрузка json файла с легендой
-        pprint(ava_list)
+            ava_list_uniq.append(ava_name)
+
+            ava_list[counter] = [
+                {"file_name": ava_name, "size": ava_size}]  # Добавление значений в словарь для json файла с легендой
+        ya.upload_file_to_disk(ava_list, ya_folder)  # Вызов функции загрузки json файла с легендой
+        # pprint(ava_list)
+        # print(ava_list_uniq)
         return ava_list
-
-
--- vk_ava_backup.py
+++ vk_ava_backup.py
@@ -1,13 +1,25 @@
-from vk_API import VkUser
+import configparser
 
-with open('vk-token.txt', 'r') as file_object:
-    vk_token = file_object.read().strip()
-# vk_token = ''    # Впишите токен к vk аккаунту (Альтернативный вариант)
-vk_user_id = ''   # Впишите id vk аккаунта
-ya_token = ""   # Впишите токен к Yandex диску
-ya_folder = 'disk:/vk_avas/'  # Укажите для загрузки существующую папку на ya.disk'е (Например: 'disk:/vk_avas/')
+from vk_API import VkUser
+from vk_API import YandexDisk
+
+config = configparser.ConfigParser()
+config.read("settings.ini")
+
+vk_token = config["VK"]["token"]
+vk_user_id = config["VK"]["user_id"]
+vk_user_screen_name = config["VK"]["user_screen_name"]
+vk_avas_count = config["VK"]["avas_count"]
+ya_token = config["Yandex_disk"]["token"]
+ya_folder = config["Yandex_disk"]["ya_folder"]
 
 if __name__ == '__main__':
+    ya_client = YandexDisk(ya_token)
+    new_folder = ya_client.folder_creation(ya_folder)
     vk_client = VkUser(vk_token)
-    ava_data = vk_client.ava_data_editor(vk_user_id, ya_token, ya_folder)
+    if not vk_user_id:
+        vk_user_id = vk_client.get_user_id(vk_user_screen_name)
+        ava_data = vk_client.ava_data_editor(vk_user_id, vk_avas_count, ya_token, ya_folder)
+    else:
+        ava_data = vk_client.ava_data_editor(vk_user_id, vk_avas_count, ya_token, ya_folder)
 
